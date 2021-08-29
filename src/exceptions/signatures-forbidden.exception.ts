f4486db746028c63420891574bd76cb11cb4632b
MorozovAlexanderr <sashamorozov309@gmail.com>
2021-08-29 13:30:56 +0300

    fix document file access

--- src/exceptions/signatures-forbidden.exception.ts
+++
@@ -1,7 +0,0 @@
-import { ForbiddenException } from '@nestjs/common';
-
-export class SignaturesForbiddenException extends ForbiddenException {
-  constructor(error?: string) {
-    super('error.signatures_forbidden', error);
-  }
-}
